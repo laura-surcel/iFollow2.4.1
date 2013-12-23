b97a7f055bba51dd34f8ec9db821eabafc909b37
Laura Surcel <laurasurcel16@gmail.com>
2013-12-23 17:15:10 +0200

    Profile page: added more functionality

---
+++ Wad.iFollow.Web/Wad.iFollow.Web/Models/ProfileModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wad.iFollow.Web.Models
+{
+    public class ProfileModel
+    {
+        public ProfileModel()
+        {
+            elements = new WallPostsModel();
+        }
+
+        public string userName { get; set; }
+        public long userId { get; set; }
+        public int postsCount { get; set; }
+        public int followersCount { get; set; }
+        public int followedCount { get; set; }
+
+        public WallPostsModel elements { get; set; }
+    }
+}
